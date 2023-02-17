555b88d4e1d9caf2997bda1f02ef1648626f1230
chidubemthedev <chukwudubem7@gmail.com>
2023-02-16 18:15:56 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+
+/**
+ * main - entry point for the program
+ *
+ * Return: 0 when program is executed
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
