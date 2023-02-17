2e100cb415d66c32b0a36134e9665fe28e9d6685
chidubemthedev <chukwudubem7@gmail.com>
2023-02-16 19:04:58 -0800

    Task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for program
+ *
+ * Return: 1 if successful
+ */
+
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful");
+	fprintf(stderr, "%s", " - Dora Korpar, 2015-10-19");
+	return (1);
+}
