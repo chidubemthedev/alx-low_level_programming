38edc6f320830315556ede8e71a6ed392ab530f2
chidubemthedev <chukwudubem7@gmail.com>
2023-02-16 17:42:18 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * function puts: outputs the inserted string
+ * returns 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
