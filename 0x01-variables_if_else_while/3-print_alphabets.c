4f710466df1d7bf86c80f5ca2ecd7246a1de8083
chidubemthedev <chukwudubem7@gmail.com>
2023-02-17 18:26:08 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: the english alphabet in small and captial
+ */
+
+int main(void)
+{
+	char c;
+	char C;
+
+	c = 'a';
+	C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
