56f5cbc87f2b14c3cd5d7206053efcfb92f9b630
chidubemthedev <chukwudubem7@gmail.com>
2023-02-21 17:50:59 -0800

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if input is an alphabet
+ * @c: the input
+ *
+ * Return: 1 if true, else 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
