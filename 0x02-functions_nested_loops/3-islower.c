5948aad1c242adeba67cb787569bf120f988b09b
chidubemthedev <chukwudubem7@gmail.com>
2023-02-21 17:28:47 -0800

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isLower - checks for lowerCase char
+ * @c: character to be checked
+ *
+ * Return: 1 if true, 0 oherwise
+ */
+
+int _isLower(int c)
+{
+	if (c >= 'a' || c <= 'z')
+	{
+		return (1);
+	}
+}
