3a2fbe87cecb8e5c0e364ef56de3ef1298f96bd6
chidubemthedev <chukwudubem7@gmail.com>
2023-02-21 17:40:04 -0800

    Task 3 fixes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isLower - checks for lowerCase char
+ * _islower - checks for lowerCase char
  * @c: character to be checked
  *
  * Return: 1 if true, 0 oherwise
  */
 
-int _isLower(int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
