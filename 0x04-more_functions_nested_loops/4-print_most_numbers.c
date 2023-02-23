534f6fedb3b67476e2d022b5f10d072f2b4d6463
chidubemthedev <chukwudubem7@gmail.com>
2023-02-23 12:39:32 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Return: numbers without 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i == 2) || (i == 4))
+		{
+			i++;
+		}
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
