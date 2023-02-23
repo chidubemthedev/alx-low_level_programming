e5610fa746cab1dea66b40094f6c6729c821224d
chidubemthedev <chukwudubem7@gmail.com>
2023-02-23 12:23:31 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - entry point to function
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: the multiplication
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
