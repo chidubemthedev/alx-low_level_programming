a67c0b41228a8642679864598e06a2d5814e42a9
chidubemthedev <chukwudubem7@gmail.com>
2023-02-27 16:50:07 -0800

    task 0:

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets a value to 98
+ * @*n: pointer as parameter
+ *
+ * Return: value as 98
+ */
+
+void reset_to_98(int *n)
+{
+	int m;
+
+	m = *n;
+
+	*m = 98;
+}
