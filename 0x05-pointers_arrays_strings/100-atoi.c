3ecbf4c09821295f67a5dae6f96ce18ede894000
chidubemthedev <chukwudubem7@gmail.com>
2023-02-27 19:34:38 -0800

    task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _atoi - entry point
+ * @s: string
+ *
+ * Return: integer from string
+ */
+
+int _atoi(char *s)
+{
+	int x;
+
+	if (!x)
+	{
+		return (0);
+	}
+
+	x = (int) s;
+	return (x);
+}
