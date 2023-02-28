4580398895ca7fa4875435c80007e4ead262559b
chidubemthedev <chukwudubem7@gmail.com>
2023-02-27 18:48:33 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - entry point
+ * @s: string to reverse
+ *
+ * Return: reversed string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] == '\0')
+	{
+		for (j = i - 1; j >= 0; j--)
+		{
+			_putchar(s[j]);
+		}
+		i++;
+	}
+	_putchar('\n');
+
+}
