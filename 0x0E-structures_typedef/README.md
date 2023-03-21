055afdb97dd08b44ab1a0285b52b9c2efc20ac76
chidubemthedev <chukwudubem7@gmail.com>
2023-03-20 19:59:09 -0700

    task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Folder for structures and typedefs
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - dog object
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char owner;
+}
+
+#endif
