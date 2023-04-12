3cbc021e484d2c8496451c6097e980b2bd7166f5
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-04-12 04:53:48 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,5 +8,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+  unsigned int number = 0;
+	int length = 0;
 
+	if (b[length] == '\0')
+		return (0);
+
+	while ((b[length] == '0') || (b[length] == '1'))
+	{
+		number <<= 1;
+		number += b[length] - '0';
+		length++;
+	}
+
+	return (number);
 }
