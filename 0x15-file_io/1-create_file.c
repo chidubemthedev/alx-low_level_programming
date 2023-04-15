3337ace1b7e095667e34e8b63d562953c1fc86b9
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-04-15 05:24:41 +0100

    task 1 fix 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,9 +5,9 @@ int _string_len(char *s);
  * create_file - creates a file
  *
  * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
+ * @text_content: NULL terminated string to write to file
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 if successful, -1 on failure
  *
  */
 int create_file(const char *filename, char *text_content)
